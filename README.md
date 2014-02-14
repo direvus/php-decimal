e0a32035acd8d0d949257d9c98751bd804dea926
Brendan Jurd <brendan.jurd@achievecorp.com.au>
2014-02-14 18:45:04 +1100

    Initial commit of minimally functional library.
    Everything tested so far seems to work as advertised.  The API could be
    enriched considerably.

--- README.md
+++ README.md
@@ -1,4 +1,141 @@
 php-decimal
 ===========
 
-An arbitrary-precision decimal library for PHP
+An arbitrary-precision decimal library for PHP.
+
+Background
+----------
+
+The PHP language only offers two numeric data types: int and float.  Neither of
+these types are suitable for a wide range of real-world problems, where exact
+arithmetic with values of arbitrary precision are required---notably, when
+working with monetary values.
+
+PHP's optional extension 'BCMath' provides some limited features in this area,
+but it is awkward to use with arbitrary precision, and it does not support
+rounding.
+
+This library uses the BCMath functions internally, but hides them behind a more
+convenient, object-oriented, and intuitive API, inspired by Python's standard
+library module 'decimal'.
+
+Installation
+------------
+
+Copy the library file 'decimal.php' to anywhere on your system you like.
+Personally I recommend something like `/usr/local/lib/php-decimal`, but it's
+totally up to you.
+
+In your PHP code, execute `include '/path/to/lib/decimal.php';` and you're
+ready to use the library.
+
+Usage
+-----
+
+One classic example of how binary floating-point values can ruin your day is
+the expression (0.1 + 0.1 + 0.1 - 0.3), which is of course zero.  Let's use
+this as a worked example.  First, using PHP's built-in float data type:
+
+    php > $n = 0.1 + 0.1 + 0.1 - 0.3;
+    php > var_dump($n);
+    float(5.5511151231258E-17)
+    
+    php > var_dump($n == 0);
+    bool(false)
+
+Unless you want to do all your comparisons using an epsilon value, binary
+floating-point is exactly this kind of trouble waiting to happen.  The next
+most obvious place to turn is the BCMath extension, which would look like:
+
+    php > bcscale(1);
+    php > $n = bcsub(bcadd(bcadd('0.1', '0.1'), '0.1'), '0.3');
+    php > var_dump($n);
+    string(3) "0.0"
+    
+    php > var_dump(bccomp($n, 0) == 0);
+    bool(true)
+
+Now we're getting the right answer, but it's an unreadable mess, and it gets
+worse if you don't know ahead of time what the precision of your inputs is
+going to be, and therefore can't choose a global value for bcscale that will be
+appropriate for all circumstances.
+
+Enter php-decimal:
+
+    php > include '/path/to/lib/decimal.php';
+    
+    php > $n = new Decimal\Decimal;
+    php > $n->increase('0.1', '0.1', '0.1', '-0.3');
+    php > var_dump((string) $n);
+    string(1) "0"
+    
+    php > var_dump($n->zero());
+    bool(true)
+
+We didn't have to lock php-decimal into any particular precision for its
+computations; it noticed that all its operands had a scale of 1, so it told
+BCMath to performs its operations at scale 1.  If any of the operands had had a
+greater scale, then we would have used that scale instead.
+
+Unfortunately, as PHP does not offer any way to hook into its built-in
+comparison and arithmetic operators, it isn't possible to use natural syntax
+like $c = $a + $b, where $a and $b are Decimal objects.  We have to make do
+with relatively ugly method calls.
+
+Internals
+---------
+
+Each instance of the Decimal class consists of:
+
+  * a sequence of decimal digits stored as a PHP string,
+  * an integer exponent, and
+  * a boolean indicating whether the number is negative.
+
+Each Decimal value represents the real number *n* such that:
+
+    n = [-]digits * (10 ^ exponent)
+
+The number 7500 would be therefore represented internally by the values
+('75', '2', false), respectively, as 7500 = 75 x 10^2.
+
+It can be seen that, for every real number which has a finite representation in
+decimal form, there are infinitely many possible Decimal representations, but
+only one representation which uses the minimal number of decimal digits, which
+is called the 'normal' or 'canonical' representation.
+
+Decimals can be initialised from other Decimal instances, strings describing
+numbers, integers and floats (with the caveat that if you initialise from a
+float you might end up exposing yourself to precisely the kind of mischief this
+library was created to avoid).
+
+In terms of performance, this library is probably pretty awful and should
+really only be used as a last resort.  If at all possible, perform all
+computations in your database or in some other programming environment, and
+leave PHP blissfully agnostic of difficult problems like basic arithmetic.
+
+Standards
+---------
+
+This library has some superficial resemblances to the IEEE 854 and 754
+standards, but does not comply, nor does it attempt to comply, with these
+standards.  It would be possible to extend the library into compliance, but at
+this time the author has no intention of doing so.
+
+In particular, php-decimal does not attempt to represent abstract numeric
+concepts like 'Not a Number', infinity or negative infinity.
+
+License
+-------
+
+php-decimal is released under the "BSD 2-clause license", the full text of
+which can be found in the LICENSE file at the top level of the repository.
+
+Acknowledgements
+----------------
+
+This library was heavily inspired by the [decimal][1] module of Python's standard
+library, and the [numeric data type][2] of PostgreSQL.
+
+  [1]: http://docs.python.org/2/library/decimal.html
+  [2]: http://www.postgresql.org/docs/current/static/datatype-numeric.html#DATATYPE-NUMERIC-DECIMAL
+
---
+++ decimal.php
@@ -0,0 +1,537 @@
+<?php
+/*
+ * Arbitrary-precision exact numeric library for PHP.
+ *
+ * This library wraps the PHP 'bcmath' extension, offering a more natural and 
+ * convenient API by automatically determining a sensible scale to use for 
+ * arithmetic results, and by automatically removing superfluous precision from 
+ * numbers.
+ *
+ * See the README.md file in the top-level directory of this library for more 
+ * details.
+ *
+ * This library is licensed under the "BSD 2-clause" open source license, the 
+ * full text of which is available in the file 'LICENSE' in the top-level 
+ * directory of this library.
+ */
+namespace Decimal;
+
+const ZERO = '0';
+
+class Decimal {
+    public $digits;
+    public $exponent = 0;
+    public $negative = false;
+    public static $raw_formatter;
+
+    public function __construct($value=0){
+        if($value instanceof Decimal){
+            $this->copy($value);
+        }else{
+            $clean = clean_value($value);
+            if($clean[0] == '-'){
+                $this->negative = true;
+                $clean = substr($clean, 1);
+            }
+            // If the value contains an exponent specifier, parse and remove 
+            // it.
+            $clean = strtolower($clean);
+            $pos = strrpos($clean, 'e');
+            if($pos !== false){
+                $this->exponent = (int) substr($clean, $pos + 1);
+                $clean = substr($clean, 0, $pos);
+            }
+            // Remove the period and decrease the exponent by one for each 
+            // digit following the period.
+            $pos = strpos($clean, '.');
+            if($pos !== false){
+                $clean = substr($clean, 0, $pos) . substr($clean, $pos + 1);
+                $this->exponent += ($pos - strlen($clean));
+            }
+            // Discard leading zeroes.
+            $clean = ltrim($clean, ZERO);
+            // Remove trailing zeroes and increase the exponent by one for each 
+            // digit removed.
+            $len = strlen($clean);
+            $clean = rtrim($clean, ZERO);
+            $this->exponent += ($len - strlen($clean));
+            if($clean == ''){
+                $this->digits = ZERO;
+            }else{
+                $this->digits = $clean;
+            }
+        }
+    }
+
+    /*
+     * Make this Decimal identical to $decimal by directly copying its 
+     * properties.
+     */
+    public function copy(Decimal $decimal){
+        $this->digits   = $decimal->digits;
+        $this->exponent = $decimal->exponent;
+        $this->negative = $decimal->negative;
+    }
+
+    /*
+     * Return the number of digits after the decimal point required to fully 
+     * represent this value.
+     */
+    public function getScale(){
+        if($this->exponent >= 0){
+            return 0;
+        }else{
+            return -$this->exponent;
+        }
+    }
+
+    /*
+     * Compare this Decimal with a value, and return:
+     *     -1 if the instance is less than the given value,
+     *     0  if the instance is equal to the given value, or
+     *     1  if the instance is greater than the given value.
+     */
+    public function compare($value){
+        $decimal = make($value);
+        $scale = \max($this->getScale(), $decimal->getScale());
+        return bccomp($this, $decimal, $scale);
+    }
+
+    public function equals($value){
+        return ($this->compare($value) == 0);
+    }
+
+    public function greaterThan($value){
+        return ($this->compare($value) > 0);
+    }
+
+    public function lessThan($value){
+        return ($this->compare($value) < 0);
+    }
+
+    public function zero(){
+        return $this->equals(0);
+    }
+
+    public function positive(){
+        return $this->greaterThan(0);
+    }
+
+    public function negative(){
+        return $this->lessThan(0);
+    }
+
+    public function eq($value){
+        return $this->equals($value);
+    }
+
+    public function lt($value){
+        return $this->lessThan($value);
+    }
+
+    public function gt($value){
+        return $this->greaterThan($value);
+    }
+
+    public function ge($value){
+        return ($this->compare($value) >= 0);
+    }
+
+    public function le($value){
+        return ($this->compare($value) <= 0);
+    }
+
+    /*
+     * Return the absolute value of this Decimal as a new Decimal.
+     */
+    public function abs(){
+        $result = new Decimal($this);
+        $result->negative = false;
+        return $result;
+    }
+
+    /*
+     * Return the negation of this Decimal as a new Decimal.
+     */
+    public function negation(){
+        $result = new Decimal($this);
+        $result->negative = !$this->negative;
+        return $result;
+    }
+
+    /*
+     * Add $value to this Decimal and return the sum as a new Decimal.
+     */
+    public function add($value, $scale=null){
+        $decimal = make($value);
+        $scale = result_scale($this, $decimal, $scale);
+        return new Decimal(bcadd($this, $decimal, $scale));
+    }
+
+    /*
+     * Subtract $value from this Decimal and return the difference as a new 
+     * Decimal.
+     */
+    public function subtract($value, $scale=null){
+        $decimal = make($value);
+        $scale = result_scale($this, $decimal, $scale);
+        return new Decimal(bcsub($this, $decimal, $scale));
+    }
+
+    public function sub($value, $scale=null){
+        return $this->subtract($value, $scale);
+    }
+
+    /*
+     * Multiply this Decimal by $value and return the product as a new Decimal.
+     */
+    public function multiply($value, $scale=null){
+        $decimal = make($value);
+        if(!scale_valid($scale)){
+            $scale = $this->getScale() + $decimal->getScale();
+        }
+        return new Decimal(bcmul($this, $decimal, $scale));
+    }
+
+    public function mul($value, $scale=null){
+        return $this->multiply($value, $scale);
+    }
+
+    /*
+     * Divide this Decimal by $value and return the quotient as a new Decimal.
+     */
+    public function divide($value, $scale=null){
+        $decimal = make($value);
+        $scale = result_scale($this, $decimal, $scale);
+        return new Decimal(bcdiv($this, $decimal, $scale));
+    }
+
+    public function div($value, $scale=null){
+        return $this->divide($value, $scale);
+    }
+
+    /*
+     * Return the inverse (1/x) of this Decimal as a new Decimal.
+     */
+    public function inverse($scale=null){
+        if(!scale_valid($scale)){
+            $scale = $this->getScale();
+        }
+        $num = one();
+        return $num->divide($this, $scale);
+    }
+
+    /*
+     * Increase/decrease this Decimal in-place by the given argument(s).
+     *
+     * Iterable arguments are processed recursively.
+     */
+    public function increase(){
+        $args = func_get_args();
+        foreach($args as $arg){
+            if(is_array($arg) || $arg instanceof Traversable){
+                foreach($args as $element){
+                    $this->increase($element);
+                }
+            }else{
+                $this->copy($this->add($arg));
+            }
+        }
+    }
+
+    public function decrease($value){
+        $args = func_get_args();
+        foreach($args as $arg){
+            if(is_array($arg) || $arg instanceof Traversable){
+                foreach($args as $element){
+                    $this->decrease($element);
+                }
+            }else{
+                $this->copy($this->sub($arg));
+            }
+        }
+    }
+
+    /*
+     * Flip the sign of this Decimal, and return whether the result is 
+     * negative.
+     */
+    public function negate(){
+        $this->negative = !$this->negative;
+        return $this->negative;
+    }
+
+    /*
+     * Return a new Decimal which represents this value using the minimum 
+     * possible number of digits without any loss of precision.
+     */
+    public function compress(){
+        $result = clone $this;
+        $len = strlen($result->digits);
+        $result->digits = ltrim($result->digits, ZERO);
+        $newlen = strlen($result->digits);
+        if($newlen > 0){
+            $result->exponent -= $len - $newlen;
+        }else{
+            $result->digits = ZERO;
+            $result->exponent = 0;
+            return $result;
+        }
+        $result->digits = rtrim($result->digits, ZERO);
+        $result->exponent += $newlen - strlen($result->digits);
+        return $result;
+    }
+
+    /*
+     * Return a new Decimal which expresses this value at the given exponent.
+     *
+     * If this Decimal cannot be fully expressed using the target exponent, 
+     * round the result using $method, which has the same meaning as in PHP's 
+     * built-in round function.
+     */
+    public function quantize($exponent, $method=PHP_ROUND_HALF_UP){
+        $result = $this->compress();
+        $count = $result->exponent - $exponent;
+        if($exponent < $result->exponent){
+            $result->digits .= zeroes($count);
+            $result->exponent = $exponent;
+        }elseif($exponent > $result->exponent){
+            if($result->exponent < 0){
+                $last = substr($result->digits, $count - 1, 1);
+                $roundoff = new Decimal;
+                if($method == PHP_ROUND_HALF_DOWN ||
+                        ($method == PHP_ROUND_HALF_EVEN && ($last % 2 == 0)) ||
+                        ($method == PHP_ROUND_HALF_ODD && ($last % 2 != 0))){
+                    $roundoff->digits = '4';
+                }else{
+                    $roundoff->digits = '5';
+                }
+                $roundoff->exponent = ($exponent - 1);
+                $roundoff->negative = $result->negative;
+                $result->increase($roundoff);
+                $result->digits = substr($result->digits, 0, $count);
+                if(strlen($result->digits) == 0){
+                    $result->digits = ZERO;
+                }
+            }else{
+                $result->digits = zeroes(-$count) . $result->digits;
+            }
+            $result->exponent = $exponent;
+        }
+        return $result;
+    }
+
+    /*
+     * Return a new Decimal which has been rounded to $places decimal places, 
+     * using $method per PHP's built-in round function.
+     */
+    public function round($places, $method=PHP_ROUND_HALF_UP){
+        return $this->quantize(\min(0, -$places), $method);
+    }
+
+    public function __toString(){
+        if(!self::$raw_formatter instanceof Formatter){
+            self::$raw_formatter = new Formatter(null, '', '.');
+        }
+        return self::$raw_formatter->format($this);
+    }
+
+    public function toFloat(){
+        return (float) (string) $this;
+    }
+
+    public function format($places=null, $grouping='', $radix_mark='.'){
+        $f = new Formatter($places, $grouping, $radix_mark);
+        return $f->format($this);
+    }
+}
+
+class Formatter {
+    public $places;
+    public $grouping;
+    public $radix_mark;
+
+    public function __construct($places=null, $grouping='', $radix_mark='.'){
+        $this->places = $places;
+        $this->grouping = $grouping;
+        $this->radix_mark = $radix_mark;
+    }
+
+    public function format($decimal){
+        if($decimal === '' || $decimal === null){
+            $decimal = 0;
+        }
+        if($decimal instanceof Decimal){
+            $decimal = $decimal->compress();
+        }else{
+            $decimal = new Decimal($decimal);
+        }
+        if($this->places !== null && $this->places != $decimal->getScale()){
+            $decimal = $decimal->round($this->places);
+        }
+        if($decimal->exponent >= 0){
+            $fill = zeroes($decimal->exponent);
+            $intpart = $decimal->digits . $fill;
+            $fracpart = '';
+        }else{
+            $intpart = substr($decimal->digits, 0, $decimal->exponent);
+            $fracpart = substr($decimal->digits, $decimal->exponent);
+            $len = strlen($fracpart);
+            $scale = $decimal->getScale();
+            if($len < $scale){
+                $fracpart = zeroes($scale - $len) . $fracpart;
+            }
+        }
+        if($intpart == ''){
+            $intpart = ZERO;
+        }
+        $grouplen = strlen($this->grouping);
+        if($grouplen > 0){
+            for($i = 3; $i < strlen($intpart); $i += 3 + $grouplen){
+                $intpart = substr_replace($intpart, $this->grouping, -$i, 0);
+            }
+        }
+        $result = '';
+        if($decimal->negative){
+            $result = '-';
+        }
+        $result .= $intpart;
+        if(strlen($fracpart) > 0){
+            $result .= $this->radix_mark . $fracpart;
+        }
+        return $result;
+    }
+}
+
+class MoneyFormatter extends Formatter {
+    public function __construct($places=2, $grouping='', $radix_mark='.'){
+        parent::__construct($places, $grouping, $radix_mark);
+    }
+}
+
+class GroupedMoneyFormatter extends MoneyFormatter {
+    public function __construct($places=2, $grouping=',', $radix_mark='.'){
+        parent::__construct($places, $grouping, $radix_mark);
+    }
+}
+
+/*
+ * Return a Decimal instance from the given value.
+ *
+ * If the value is already a Decimal instance, then return it unmodified.  
+ * Otherwise, create a new Decimal instance from the given value and return it.
+ */
+function make($value){
+    if($value instanceof Decimal){
+        return $value;
+    }else{
+        return new Decimal($value);
+    }
+}
+
+/*
+ * Return the given number as a string with irrelevant characters removed.
+ *
+ * All characters other than digits, hyphen, period and the exponent marker 'e' 
+ * are removed entirely, and any contiguous series of zeroes after a period at 
+ * the end of the string are also removed.
+ *
+ * Raise an Exception if the value is not a valid numeric representation.  We 
+ * make no attempt to interpret exponential notation.
+ */ 
+function clean_value($value){
+    if(is_int($value) || is_float($value)){
+        return (string) $value;
+    }else{
+        $clean = preg_replace('/[^\d.eE-]/', '', $value);
+        if(strpos($clean, '.') !== false){
+            $clean = rtrim(rtrim($clean, ZERO), '.');
+        }
+        if(!preg_match('/^-?\d+(?:\.\d*)?(?:[eE]-?\d*)?$/', $clean)){
+            throw new Exception(
+                "Invalid Decimal value '$value'; " .
+                "must contain at least one digit, optionally preceeded " .
+                "by a sign specifier, optionally followed by " .
+                "a period and a fractional part, optionally followed by E " .
+                "and an integer exponent.");
+        }
+        return $clean;
+    }
+}
+
+/*
+ * Return the greatest of the arguments.
+ */
+function max(){
+    $args = func_get_args();
+    $result = null;
+    foreach($args as $arg){
+        $dec = make($arg);
+        if($result === null || $result->lt($dec)){
+            $result = $dec;
+        }
+    }
+    return $result;
+}
+
+/*
+ * Return the lowest of the arguments.
+ */
+function min(){
+    $args = func_get_args();
+    $result = null;
+    foreach($args as $arg){
+        $dec = make($arg);
+        if($result === null || $result->gt($dec)){
+            $result = $dec;
+        }
+    }
+    return $result;
+}
+
+/*
+ * Return whether $scale is valid as a decimal operation scale.
+ */
+function scale_valid($scale){
+    return (is_int($scale) && $scale > 0);
+}
+
+/*
+ * Return basic values as Decimal.
+ */
+function zero(){
+    global $_decimal_zero;
+    if(!$_decimal_zero instanceof Decimal){
+        $_decimal_zero = new Decimal(0);
+    }
+    return $_decimal_zero;
+}
+
+function one(){
+    global $_decimal_one;
+    if(!$_decimal_one instanceof Decimal){
+        $_decimal_one = new Decimal(1);
+    }
+    return $_decimal_one;
+}
+
+/*
+ * Return an appropriate scale for an arithmetic operation on two Decimals.
+ *
+ * If $scale is specified and is a valid positive integer, return it.  
+ * Otherwise, return the higher of the scales of the operands.
+ */
+function result_scale(Decimal $a, Decimal $b, $scale=null){
+    if(!scale_valid($scale)){
+        $scale = \max($a->getScale(), $b->getScale());
+    }
+    return $scale;
+}
+
+/*
+ * Return a string of zeroes of length $length.
+ */
+function zeroes($length){
+    return str_repeat(ZERO, $length);
+}
+?>
